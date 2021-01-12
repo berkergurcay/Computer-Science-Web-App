cc437b73669d11f800edc6497dc60ed3aa6e6bda
SadikhovEmin <eemin.sadykhov@gmail.com>
2021-01-12 20:47:34 +0400

    Add Header

---
+++ ozu-cs/src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Header.css'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    BrowserRouter
+  } from "react-router-dom";
+
+
+class Header extends React.Component {
+    render() {
+        return  <div class="main">
+            <div id="ust">
+                
+                <BrowserRouter>
+                    <Link to='/'><img id="logo" src="https://www.ozyegin.edu.tr/sites/default/files/logo-tr.png" alt=""/></Link>
+                </BrowserRouter>
+
+            </div>
+        </div>
+    }
+}
+
+export default Header;
---
+++ ozu-cs/src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    BrowserRouter
+  } from "react-router-dom";
+
+class NavBar extends React.Component {
+    render() {
+        return  <nav></nav>
+    }
+}
+
+export default NavBar;
