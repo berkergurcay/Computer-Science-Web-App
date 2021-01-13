bee1b54c92c7107de89b28ea6889353eb158b935
berkergurcay <berker.gurcay@ozu.edu.tr>
2021-01-13 21:11:07 +0300

    Solve issues in register class

--- ozu-cs/src/components/Register.js
+++ ozu-cs/src/components/Register.js
@@ -1,64 +1,102 @@
 import React from 'react'
 import './Register.css'
 
+
+
+function validate(name, surname, email, snumber, number){
+
+    const errors = [];
+
+    if (name === "" || surname === "" || email === "" || snumber === "" || number === "") {
+       errors.push('Please fill all necessary fields');
+    }
+   
+    if (snumber.toLowerCase()[0] != 's') {
+        errors.push('Student number should start with S');
+    }
+
+    if (!email.endsWith('@ozu.edu.tr')) {
+        errors.push('Please enter your OzU email address');
+        
+    }
+    if(snumber.length != 7) {
+        errors.push("Student number must be 7 digits");
+
+    }
+    
+    if (number.length != 11){
+        errors.push("Phone number must be 11 digits starting from 0");
+    }
+
+    return errors;
+}
+
 class Register extends React.Component {
     constructor(props){
         super(props);
-        this.store = this.store.bind(this);
-    }
+        this.state = {
+            name: "",
+            surname:"",
+            email:"",
+            snumber:"",
+            number:"",
 
-    store(){
-
-        var student = {
-            firstname: document.getElementById("name").value,
-            surname: document.getElementById("surname").value,
-            email: document.getElementById("email").value,
-            snumber: document.getElementById("snumber").value,
-            number: document.getElementById("number").value,
-            class: document.getElementById("class").value,
+            errors: []
         };
-        
-        if (student.firstname == "" || student.surname == "" || student.email == "" || student.snumber == "" || student.number == "") {
-            alert('Please fill all necessary fields');
-            // document.getElementById("action-form").action="/register"
-            // return false;
-        }
-       
-        if (student.snumber.toLowerCase()[0] != 's') {
-            alert('Student number should start with S');
-            // document.getElementById("action-form").action="/register"
 
-            // return false;
-        }
-
-        if (!student.email.endsWith('@ozu.edu.tr')) {
-            alert ('Please enter your OzU email address');
-            // document.getElementById("action-form").action="/register"
-
-            // return false;
-        }
-        
-
-        localStorage.setItem(student.snumber, JSON.stringify(student));
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        const {name, surname, email, snumber, number} = this.state;
+
+        const errors = validate(name, surname, email, snumber, number);
+        if (errors.length > 0){
+            this.setState({errors});
+            return;
+        }else{
+            var student = {
+                "firstname": name,
+                "surname": surname,
+                "email": email,
+                "snumber": snumber,
+                "Number": number,
+                "class": document.getElementById("class").value,
+            };
+            this.setState({errors: []})
+            localStorage.setItem(student.snumber,JSON.stringify(student));
+            document.getElementById("action-form").action = "/register"
+        }
+
+    }
+
+ 
+        
+
 
     render() {
+        const { errors } = this.state;
         return <div id="register">
-            <form onSubmit={this.store} className="form-login"  method="GET" id="action-form"> 
+            <form onSubmit={this.handleSubmit} className="form-login" id="action-form">
+                {errors.map(error => (
+                    <p key= {error}>Error: {error}</p>
+                ))} 
                 <label htmlFor="name">Name</label>
-                <input type="text" id="name" name="firstname" placeholder="Name"></input>
+                <input type="text" value={this.state.name} onChange={evt => this.setState({ name: evt.target.value}) }id="name" name="firstname" placeholder="Name"></input>
 
                 <label htmlFor="surname">Surname</label>
-                <input type="text" id="surname" name="surname" placeholder="Surname"></input>
+                <input type="text" value={this.state.surname} onChange={evt => this.setState({ surname: evt.target.value}) } id="surname" name="surname" placeholder="Surname"></input>
 
                 <label htmlFor="email">Email</label>
-                <input type="text" id="email" name="email" placeholder="Email"></input>
+                <input type="text" value={this.state.email} onChange={evt => this.setState({ email: evt.target.value}) } id="email" name="email" placeholder="Email"></input>
 
                 <label htmlFor="snumber">Student Number</label>
-                <input type="text" id="snumber" name="snumber" placeholder="Student Number (starts with s)"></input>
+                <input type="text" value={this.state.snumber} onChange={evt => this.setState({ snumber: evt.target.value}) } id="snumber" name="snumber" placeholder="Student Number (starts with s)"></input>
 
                 <label htmlFor="number">Number</label>
-                <input type="text" id="number" name="number" placeholder="Phone Number"></input>
+                <input type="text" value={this.state.number} onChange={evt => this.setState({ number: evt.target.value}) } id="number" name="number" placeholder="Phone Number"></input>
             
                 <label htmlFor="class">Class</label>
                 <select id="class" name="country">
